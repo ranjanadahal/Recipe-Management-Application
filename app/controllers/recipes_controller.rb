406dc0b120fdb6584cf58bc12ca7c8ba0d1399ad
Ranjana Dahal <ranjanadahal17@gmail.com>
2020-03-13 02:33:51 -0500

    add bootstrap and nav bar on index page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,8 @@
 class RecipesController < ApplicationController
     before_action :find_recipe, only: [:show, :edit, :update, :destroy]
     def index
-        @recipe = Recipe.all.order("created_at DESC")
-    end
+		@recipe = Recipe.all.order("created_at DESC")
+	end
     def show
     end
     def new
