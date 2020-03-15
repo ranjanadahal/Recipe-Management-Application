7a22bf541ca06af0baece731bfd81d2fe2cd3e48
Ranjana Dahal <ranjanadahal17@gmail.com>
2020-03-15 05:59:32 -0500

    search recipe by title and ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,6 +10,6 @@ class Recipe < ApplicationRecord
     mount_uploader :image, ImageUploader
 
     def self.search_by(search_term)
-        where("LOWER(title) LIKE :search_term" ,search_term: "%#{search_term.downcase}%")
+        Recipe.joins(:ingredients).where("LOWER(title) LIKE :search_term OR LOWER(name) LIKE :search_term", search_term:"%#{search_term.downcase}%")
     end
 end
